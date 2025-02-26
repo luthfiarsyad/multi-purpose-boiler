5d574f461adc921b74e4af6168669a1773ca3828
luthfiarsyad <luthfiarsyad68@gmail.com>
2025-02-26 16:35:20 +0700

    fix(processtime pointing random value):fix process time so it can show realtime in ms, not pointing random value

--- internal/infra/handler/not_found_handler.go
+++ internal/infra/handler/not_found_handler.go
@@ -2,7 +2,9 @@ package handler
 
 import (
 	"base-app/pkg/logger"
+	"context"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,8 +12,11 @@ import (
 // This handler is handling not found path, so everytime the requested path is not available, it will directing here.
 func NotFoundHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		startTime := time.Now()
+		ctx := c.Request.Context()
+		ctx = context.WithValue(ctx, logger.StartTimeKey, startTime)
 		// Log the error
-		logger.LogError(c.Request.Context(), logger.LogEvent{
+		logger.LogError(ctx, logger.LogEvent{
 			HTTPStatus: http.StatusNotFound,
 			Message:    "Resource not found",
 			Data:       gin.H{"path": c.Request.URL.Path},
